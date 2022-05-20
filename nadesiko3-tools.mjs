2f595571df3a653cbc3c90ec6e421cee1221de41
kujirahand <web@kujirahand.com>
2022-05-20 14:55:14 +0900

    キー送信を追加

--- nadesiko3-tools.mjs
+++ nadesiko3-tools.mjs
@@ -1,4 +1,5 @@
 import clipboardy from 'clipboardy'
+import sendkeysJS from 'sendkeys-js'
 
 export default {
   // @クリップボード操作
@@ -17,6 +18,25 @@ export default {
     pure: true,
     fn: function (v) {
       clipboardy.writeSync(v)
+    },
+    return_none: true
+  },
+  // @マウスとキーボード操作
+  'キー送信': { // @Sのキーを送信 // @きーそうしん
+    type: 'func',
+    josi: [['を', 'の']],
+    pure: true,
+    fn: function (s) {
+      sendkeysJS.sendKeys(s)
+    },
+    return_none: true
+  },
+  '窓アクティブ': { // @Sの窓をアクティブにする // @まどあくてぃぶ
+    type: 'func',
+    josi: [['を', 'の']],
+    pure: true,
+    fn: function (s) {
+      sendkeysJS.activate(s)
     },
     return_none: true
   }
